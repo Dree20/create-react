0a60be544b089bd3619e9bba1c5e990cf7a2a957
Dree20 <dreannaad@gmail.com>
2025-12-27 18:30:15 -0500

    Added axios integration so it shows the weather of a city

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,12 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World</h1>
+        <h1>Hello from React JS</h1>
+        <Weather city="Tokyo" />
       </header>
     </div>
   );
---
+++ my-app/src/Weather.js
@@ -0,0 +1,24 @@
+import React from "react";
+import axios from "axios";
+import { ThreeDots } from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(`The weather in ${props.city}  is ${response.data.main.temp}°C`);
+  }
+  let apiKey = "743bee57fddbfaf52447193a87d5dd25";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+  return (
+    <ThreeDots
+      height="80"
+      width="80"
+      radius="9"
+      color="#4fa94d"
+      ariaLabel="three-dots-loading"
+      wrapperStyle={{ margin: "20px" }}
+      wrapperClass="custom-loader"
+      visible={true}
+    />
+  );
+}
